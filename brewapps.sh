2d68f470457590b49648a01a39f7c029ab61337c
Daniel Fernandez Rico <daniel.fernandez@localistico.com>
2022-07-11 14:18:49 +0200

    Updated brewfiles

---
+++ brewapps.sh
@@ -0,0 +1,48 @@
+# Apps
+brew install --cask spotify
+brew install --cask calibre
+brew install --cask vlc
+brew install --cask appcleaner
+brew install --cask rectangle
+brew install --cask max
+brew install --cask alfred
+brew install --cask visual-studio-code
+brew install --cask iterm2
+brew install --cask fork
+brew install --cask figma
+brew install --cask fantastical
+brew install --cask firefox
+brew install --cask obs
+brew install --cask topnotch
+brew install --cask slack
+brew install --cask font-fira-code-nerd-font
+
+# MAS Apps
+brew install mas
+mas install 411643860  # DaisyDisk       (4.21.4)
+mas install 1444383602 # GoodNotes       (5.8.2)
+mas install 425424353  # The Unarchiver  (4.3.3)
+mas install 1487937127 # Craft           (2.1.1)
+mas install 1055511498 # Day One         (7.0.1)
+mas install 904280696  # Things          (3.15.12)
+mas install 1365531024 # 1Blocker        (4.1.3)
+mas install 409203825  # Numbers         (11.2)
+mas install 953286746  # Darkroom        (5.8.4)
+mas install 1289583905 # Pixelmator Pro  (2.3.5)
+mas install 668208984  # GIPHY CAPTURE   (4.2)
+
+# Formulae
+brew install stow
+brew install autojump
+brew install exa
+brew install ffmpeg
+brew install fortune
+brew install fzf
+brew install htop
+brew install lolcat
+brew install neofetch
+brew install neovim
+brew install pfetch
+brew install ripgrep
+brew install yt-dlp
+
