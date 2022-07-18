3681501b2da317c5d5ab4aba634458aa51f44529
Daniel Fernandez Rico <daniel.fdez@pm.me>
2022-07-18 09:42:57 +0200

    Fixed oh-my-zsh plugins install process

--- config.sh
+++ config.sh
@@ -3,6 +3,8 @@ if hash brew 2>/dev/null; then
     echo "Brew is already installed"
 else
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/blayhem/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 # 2. Installing apps
@@ -15,22 +17,22 @@ stow vim
 stow zsh
 # TODO: finish config bee, iTerm...
 
-if [ "$ZSH_CUSTOM" == "" ]; then
-    ZSH_CUSTOM="~/.oh-my-zsh/custom"
-fi
-
 # 4. oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # 5. oh-my-zsh plugins
+if [ "$ZSH_CUSTOM" == "" ]; then
+    ZSH_CUSTOM="~/.oh-my-zsh/custom"
+fi
+
 echo "Installing plugins to" $ZSH_CUSTOM
 # https://github.com/spaceship-prompt/spaceship-prompt 
 git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 # https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/INSTALL.md
-brew install zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 # https://github.com/zsh-users/zsh-autosuggestions/blob/master/INSTALL.md
-brew install zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # https://github.com/powerline/fonts
 git clone https://github.com/powerline/fonts.git --depth=1
