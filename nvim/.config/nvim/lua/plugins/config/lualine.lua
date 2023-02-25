c237b6b9cc6a44a522e51bdb34bd52868b86264b
Daniel Fernandez Rico <daniel.fdez@pm.me>
2023-02-25 19:09:47 +0100

    gruvbox theme for lualine (cherry pick)

--- nvim/.config/nvim/lua/plugins/config/lualine.lua
+++ nvim/.config/nvim/lua/plugins/config/lualine.lua
@@ -1,71 +1,66 @@
 -- lualine: vim airline/statusline replacement
 -- https://github.com/nvim-lualine/lualine.nvim
-
--- Bubbles config for lualine
--- Author: lokesh-krishna
--- MIT license, see LICENSE for more details.
-
-local colors = {
-  -- TODO: theming
-  blue   = '#80a0ff',
-  cyan   = '#79dac8',
-  black  = '#080808',
-  white  = '#c6c6c6',
-  red    = '#ff5189',
-  violet = '#d183e8',
-  grey   = '#303030',
-}
-
-local bubbles_theme = {
-  normal = {
-    a = { fg = colors.black, bg = colors.violet },
-    b = { fg = colors.white, bg = colors.grey },
-    c = { fg = colors.black, bg = colors.black },
-  },
-  insert = { a = { fg = colors.black, bg = colors.blue } },
-  visual = { a = { fg = colors.black, bg = colors.cyan } },
-  replace = { a = { fg = colors.black, bg = colors.red } },
-  inactive = {
-    a = { fg = colors.white, bg = colors.black },
-    b = { fg = colors.white, bg = colors.black },
-    c = { fg = colors.black, bg = colors.black },
-  },
-}
-
 return {
   'nvim-lualine/lualine.nvim',
   -- dependencies = { 'kyazdani42/nvim-web-devicons', opt = true }
   -- added by neotree so not necessary here
   config = function()
-    -- Bubbles config goes here
-    require('lualine').setup {
+    -- custom config for gruvbox-themed lualine
+    local lualine_c = {}
+    local ok, noice = pcall(require, "noice")
+    if ok then
+      lualine_c = { -- show macro recording
+        {
+          noice.api.statusline.mode.get,
+          cond = noice.api.statusline.mode.has,
+          color = { fg = "#ff9e64" },
+        },
+      }
+    end
+
+    local lualine_ok, lualine = pcall(require, "lualine")
+    if not lualine_ok then
+      return
+    end
+
+    local custom_gruvbox = require("lualine.themes.gruvbox")
+
+    -- Change the background of lualine_c section for normal mode
+    custom_gruvbox.normal.c.bg = require("lualine.utils.utils").extract_highlight_colors("Normal", "bg")
+    custom_gruvbox.insert.c.bg = require("lualine.utils.utils").extract_highlight_colors("Normal", "bg")
+    custom_gruvbox.visual.c.bg = require("lualine.utils.utils").extract_highlight_colors("Normal", "bg")
+    custom_gruvbox.replace.c.bg = require("lualine.utils.utils").extract_highlight_colors("Normal", "bg")
+    custom_gruvbox.command.c.bg = require("lualine.utils.utils").extract_highlight_colors("Normal", "bg")
+    custom_gruvbox.inactive.c.bg = require("lualine.utils.utils").extract_highlight_colors("Normal", "bg")
+
+    require('lualine').setup({
       options = {
-        theme = bubbles_theme,
-        component_separators = '|',
-        section_separators = { left = '', right = '' },
+        icons_enabled = true,
+        globalstatus = true, -- show only one statusline in splits needs laststatus=3
+        theme = custom_gruvbox,
+        component_separators = "|",
+        section_separators = { left = "", right = "" },
       },
       sections = {
-        lualine_a = {
-          { 'mode', separator = { left = '' }, right_padding = 2 },
-        },
-        lualine_b = { 'filename', 'branch' },
-        lualine_c = { 'fileformat' },
+        lualine_a = { { "mode", separator = { left = "", right = "" } } },
+        lualine_b = { "filename", "branch", "diff", "diagnostics" },
+        lualine_c = lualine_c,
         lualine_x = {},
-        lualine_y = { 'filetype', 'progress' },
-        lualine_z = {
-          { 'location', separator = { right = '' }, left_padding = 2 },
-        },
+        lualine_y = { "filetype", "progress" },
+        lualine_z = { { "location", separator = { left = "", right = "" } } },
       },
       inactive_sections = {
-        lualine_a = { 'filename' },
+        lualine_a = { "filename" },
         lualine_b = {},
         lualine_c = {},
         lualine_x = {},
         lualine_y = {},
-        lualine_z = { 'location' },
+        lualine_z = { "location" },
       },
       tabline = {},
+      winbar = {},
+      inactive_winbar = {},
       extensions = {},
-    }
+    })
   end
 }
