4e9e491e64e05464925955e5d15477ae74d6d21e
Daniel Fernandez Rico <daniel.fdez@pm.me>
2023-03-01 17:09:04 +0100

    telescope file search

--- nvim/.config/nvim/lua/plugins/config/telescope.lua
+++ nvim/.config/nvim/lua/plugins/config/telescope.lua
@@ -2,5 +2,19 @@
 return {
   'nvim-telescope/telescope.nvim',
   tag = '0.1.0',
-  dependencies = { { 'nvim-lua/plenary.nvim' } }
+  dependencies = { { 'nvim-lua/plenary.nvim' } },
+  config = function()
+    function TelescopeFileSearch()
+      local input_string = vim.fn.input("Search For > ")
+      if (input_string == '') then
+        return
+      end
+      require("telescope.builtin").grep_string({
+        search = input_string,
+      })
+    end
+
+    vim.keymap.set('n', '<C-f>', ':lua TelescopeFileSearch()<CR>', { noremap = false, silent = false });
+    -- :Telescope live_grep <CR> could be a good alternative
+  end
 }
